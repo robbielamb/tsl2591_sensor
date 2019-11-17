a1035d485f15c0bdfdf06c62aefea3e1d8a666d9
Robbie Lamb <lamb@eccentriclamb.com>
2019-11-17 11:12:48 -0700

    First pass at reading lux.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use rust_gpiozero::*;
 
-use i2cdev::core::*;
-use i2cdev::linux::{LinuxI2CDevice, LinuxI2CError};
+/* use i2cdev::core::*;
+use i2cdev::linux::{LinuxI2CDevice, LinuxI2CError};  */
+use rppal::i2c::{Error, I2c};
 
 mod tsl2591_lux {
-    use i2cdev::core::I2CDevice;
+    //use i2cdev::core::I2CDevice;
 
-    const TSL2591_ADDR: u8 = 0x79;
+    use rppal::i2c;
+    use std::error;
+    use std::fmt;
+
+    const TSL2591_ADDR: u16 = 0x79;
     const TSL2591_COMMAND_BIT: u8 = 0xA0;
     const TSL2591_ENABLE_POWEROFF: u8 = 0x00;
 
@@ -26,24 +31,206 @@ mod tsl2591_lux {
     const TSL2591_LUX_COEFD: f32 = 0.86;
 
     const TSL2591_MAX_COUNT_100MS: u16 = 0x8FFF;
-    const TSL2591_MAX_COUNT_: u16 = 0xFFFF;
+    const TSL2591_MAX_COUNT: u16 = 0xFFFF;
 
-    pub enum TSL2591Error<E> {
-        Error(E),
-        ParseError,
+    #[repr(u8)]
+    #[derive(Copy, Clone)]
+    pub enum Gain {
+        LOW = 0x00,  // 1x
+        MED = 0x10,  // 25x
+        HIGH = 0x20, // 428x
+        MAX = 0x30,  // 9876x
     }
 
-    pub struct TSL2591Lux<T: I2CDevice + Sized> {
-        i2cdev: T,
+    impl Gain {
+        fn from_u8(gain: u8) -> Gain {
+            match gain {
+                0x00 => Gain::LOW,
+                0x10 => Gain::MED,
+                0x20 => Gain::HIGH,
+                0x30 => Gain::MAX,
+                _ => panic!("Bad U89"),
+            }
+        }
     }
 
-    impl<T> TSL2591Lux<T> 
-    where
-    T: I2CDevice + Sized
-    {
-        pub fn new(mut i2cdev: T) -> Result<TSL2591Lux<T>, T::Error> {
+    #[repr(u8)]
+    #[derive(Copy, Clone)]
+    pub enum IntegrationTime {
+        I100MS = 0x00,
+        I200MS = 0x01,
+        I300MS = 0x02,
+        I400MS = 0x03,
+        I500MS = 0x04,
+        I600MS = 0x05,
+    }
+
+    #[derive(Debug)]
+    pub enum TSL2591Error {
+        I2cError(i2c::Error),
+        OverflowError,
+        RuntimeError,
+    }
+
+    impl fmt::Display for TSL2591Error {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            match *self {
+                TSL2591Error::RuntimeError => write!(f, "Unable to find device TSL2591"),
+                TSL2591Error::OverflowError => write!(f, "Overflow reading light channels"),
+                TSL2591Error::I2cError(ref err) => write!(f, "i2cError: {}", err),
+            }
+        }
+    }
+
+    impl error::Error for TSL2591Error {}
+
+    impl From<i2c::Error> for TSL2591Error {
+        fn from(err: i2c::Error) -> TSL2591Error {
+            TSL2591Error::I2cError(err)
+        }
+    }
+
+    pub struct TSL2591Lux {
+        i2cdev: i2c::I2c,
+        gain: Gain,
+        integration_time: IntegrationTime,
+    }
+
+    impl TSL2591Lux {
+        pub fn new(i2cdev: i2c::I2c) -> Result<TSL2591Lux, TSL2591Error> {
+            //let mut i2c = I2c::new()?;
+            let mut obj = TSL2591Lux {
+                i2cdev: i2cdev,
+                gain: Gain::MED,
+                integration_time: IntegrationTime::I100MS,
+            };
             //i2cdev.smbus_write_byte_data(register: u8, 0x00)?
-            Ok(TSL2591Lux{ i2cdev: i2cdev})
+            obj.i2cdev.set_slave_address(TSL2591_ADDR)?;
+
+            if obj.read_u8(TSL2591_REGISTER_DEVICE_ID)? != 0x50 {
+                return Err(TSL2591Error::RuntimeError);
+            }
+
+            obj.set_gain(obj.gain)?;
+            obj.set_integration_time(obj.integration_time)?;
+
+            obj.enable()?;
+
+            Ok(obj)
+        }
+
+        fn read_u8(&self, address: u8) -> Result<u8, TSL2591Error> {
+            let command = (TSL2591_COMMAND_BIT | address) * 0xFF;
+            Ok(self.i2cdev.smbus_read_byte(command)?)
+        }
+
+        fn write_u8(&self, address: u8, val: u8) -> Result<(), TSL2591Error> {
+            let command = (TSL2591_COMMAND_BIT | address) * 0xFF;
+            let val = val & 0xFF;
+            self.i2cdev.smbus_write_byte(command, val)?;
+            Ok(())
+        }
+
+        fn read_u16LE(&self, address: u8) -> Result<u16, TSL2591Error> {
+            let command = (TSL2591_COMMAND_BIT | address) * 0xFF;
+            Ok(self.i2cdev.smbus_read_word_swapped(command)?)
+        }
+
+        pub fn set_gain(&mut self, gain: Gain) -> Result<(), TSL2591Error> {
+            let control = self.read_u8(TSL2591_REGISTER_CONTROL)?;
+            let updated_control = (control & 0b11001111) | (gain as u8);
+
+            self.write_u8(TSL2591_REGISTER_CONTROL, updated_control)?;
+            self.gain = gain;
+            Ok(())
+        }
+
+        pub fn getGain(&self) -> Result<Gain, TSL2591Error> {
+            let control = self.read_u8(TSL2591_REGISTER_CONTROL)?;
+            Ok(Gain::from_u8(control & 0b00110000))
+            // Check to see if the saved value is what we pulled back?
+        }
+
+        pub fn set_integration_time(
+            &mut self,
+            integration_time: IntegrationTime,
+        ) -> Result<(), TSL2591Error> {
+            self.integration_time = integration_time;
+            let control = self.read_u8(TSL2591_REGISTER_CONTROL)?;
+            let updated_control = (control & 0b11111000) | (integration_time as u8);
+            self.write_u8(TSL2591_REGISTER_CONTROL, updated_control)
+        }
+
+        /// Activate the device using all the power available
+        pub fn enable(&self) -> Result<(), TSL2591Error> {
+            let command = TSL2591_ENABLE_POWERON
+                | TSL2591_ENABLE_AEN
+                | TSL2591_ENABLE_AIEN
+                | TSL2591_ENABLE_NPIEN;
+            self.write_u8(TSL2591_REGISTER_ENABLE, command)
+        }
+
+        /// Disables the device putting it in a low power mode.
+        pub fn disable(&self) -> Result<(), TSL2591Error> {
+            self.write_u8(TSL2591_REGISTER_ENABLE, TSL2591_ENABLE_POWEROFF)
+        }
+
+        fn raw_luminosity(&self) -> Result<(u16, u16), TSL2591Error> {
+            let channel0 = self.read_u16LE(TSL2591_REGISTER_CHAN0_LOW)?;
+            let channel1 = self.read_u16LE(TSL2591_REGISTER_CHAN1_LOW)?;
+
+            Ok((channel0, channel1))
+        }
+
+        pub fn full_spectrum(&self) -> Result<u32, TSL2591Error> {
+            let (chan0, chan1) = self.raw_luminosity()?;
+            let chan0 = chan0 as u32;
+            let chan1 = chan1 as u32;
+            Ok(chan1 << 16 | chan0)
+        }
+
+        pub fn infrared(&self) -> Result<u16, TSL2591Error> {
+            let (_, chan1) = self.raw_luminosity()?;
+            Ok(chan1)
+        }
+
+        /// Read the visible light
+        pub fn visible(&self) -> Result<u32, TSL2591Error> {
+            let (chan0, chan1) = self.raw_luminosity()?;
+            let chan0 = chan0 as u32;
+            let chan1 = chan1 as u32;
+            let full = (chan1 << 16) | chan0;
+            Ok(full - chan1)
+        }
+
+        pub fn lun(&self) -> Result<f32, TSL2591Error> {
+            let (chan0, chan1) = self.raw_luminosity()?;
+
+            // Compute the atime in milliseconds
+            let atime = 100.0 * (self.integration_time as u8 as f32) + 100.0;
+
+            let max_counts = match self.integration_time {
+                IntegrationTime::I100MS => TSL2591_MAX_COUNT_100MS,
+                _ => TSL2591_MAX_COUNT,
+            };
+
+            if chan0 >= max_counts || chan1 >= max_counts {
+                return Err(TSL2591Error::OverflowError);
+            };
+
+            let again = match self.gain {
+                Gain::LOW => 1.0,
+                Gain::MED => 25.0,
+                Gain::HIGH => 428.0,
+                Gain::MAX => 9876.0,
+            };
+
+            let cp1 = (atime * again) / TSL2591_LUX_DF;
+            let lux1 = (chan0 as f32 - (TSL2591_LUX_COEFB * chan0 as f32)) / cp1;
+            let lux2 =
+                ((TSL2591_LUX_COEFC * chan0 as f32) - (TSL2591_LUX_COEFD * chan1 as f32)) / cp1;
+
+            Ok(lux1.max(lux2))
         }
     }
 }
@@ -54,8 +241,12 @@ use std::time::Duration;
 fn main() {
     println!("Hello, world!");
 
+    let i2c = I2c::new().unwrap();
+
+    let luxDev = tsl2591_lux::TSL2591Lux::new(i2c).expect("Unable to open lux device");
+
     let led = LED::new(26);
-    let _i2cdev = LinuxI2CDevice::new("a path", 0x07900);
+    //let foo  = LinuxI2CDevice::new("device ", 0x23a3).unwrap();
     loop {
         led.on();
         sleep(Duration::from_secs(1));
